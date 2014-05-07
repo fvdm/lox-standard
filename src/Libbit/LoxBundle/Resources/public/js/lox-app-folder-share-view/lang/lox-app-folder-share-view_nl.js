b5a478f0f96e0bd51edd61727aa239985800714d
Pascal van Nugteren <pascal@xenotype.nl>
2014-05-07 13:54:08 +0200

    Added Dutch translations.

---
+++ src/Libbit/LoxBundle/Resources/public/js/lox-app-folder-share-view/lang/lox-app-folder-share-view_nl.js
@@ -0,0 +1,12 @@
+YUI.add('lang/lox-app-folder-share-view_nl', function (Y) {
+
+    Y.Intl.add('lox-app-folder-share-view', 'nl' , {
+        cancel: "Annuleren",
+        share: "Delen",
+        update: "Bijwerken",
+        remove_share: "Niet meer delen",
+        share_title: "Map delen met ...",
+        placeholder_invite: "Gebruikers en groepen uitnodigen.",
+        remove: "Verwijderen"
+    });
+});
