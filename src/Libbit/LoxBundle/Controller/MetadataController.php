315c21e7656ba86bf736378d5de6fbfe8c6499c4
Sven Hagemann <sven@rednose.nl>
2014-05-09 15:08:06 +0200

    Cleanup

--- src/Libbit/LoxBundle/Controller/MetadataController.php
+++ src/Libbit/LoxBundle/Controller/MetadataController.php
@@ -152,9 +152,6 @@ class MetadataController extends Controller
         $context->setGroups($groups);
         $view->setSerializationContext($context);
 
-//        var_dump($item->getId());
-//        exit;
-//
         $view->setData($item);
         $view->setFormat('json');
 
