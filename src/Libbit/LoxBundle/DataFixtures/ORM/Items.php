d0ff3cf5aa5a895a076222f5f1355eb73f30653c
Marc Bontje <marc@rednose.nl>
2014-05-12 16:04:45 +0200

    PHP 5.3 bugfix

--- src/Libbit/LoxBundle/DataFixtures/ORM/Items.php
+++ src/Libbit/LoxBundle/DataFixtures/ORM/Items.php
@@ -218,13 +218,9 @@ class Items extends AbstractFixture implements OrderedFixtureInterface, Containe
     {
         $file = new \SplFileInfo($path);
 
-        // MacOSX version of sys_get_temp_dir() returns trailingslash
-        if (sys_get_temp_dir()[strlen(sys_get_temp_dir()) - 1] === '/') {
-            $dest = sys_get_temp_dir() . $file->getBaseName();
-        } else {
-            $dest = sys_get_temp_dir() . '/' . $file->getBaseName();
-        }
+        $dest = rtrim(sys_get_temp_dir(), '/').'/'.$file->getBaseName();
 
+        // MacOSX version of sys_get_temp_dir() returns trailing slash.
         copy($path, $dest);
 
         return new UploadedFile($dest, $file->getBaseName(), null, null, null, true);
