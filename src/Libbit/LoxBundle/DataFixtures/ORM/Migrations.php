ffc71db399ea946969af338770e8f7fbd9e3a982
Marc Bontje <marc@rednose.nl>
2014-05-12 15:22:58 +0200

    SQLite fix

--- src/Libbit/LoxBundle/DataFixtures/ORM/Migrations.php
+++ src/Libbit/LoxBundle/DataFixtures/ORM/Migrations.php
@@ -34,7 +34,7 @@ class Migrations extends AbstractFixture implements OrderedFixtureInterface, Con
                 'CREATE TABLE IF NOT EXISTS `migration_versions` (' .
                 '`version` varchar(255) NOT NULL,' .
                 'PRIMARY KEY (`version`)' .
-                ') ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+                ');'
             );
         }
 
