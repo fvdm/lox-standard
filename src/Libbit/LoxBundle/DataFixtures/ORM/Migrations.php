3491d8c7669a580bd36ff5f1c9e5d10344c86f88
Marc Bontje <marc@rednose.nl>
2014-05-12 15:13:43 +0200

    Migration table fix

--- src/Libbit/LoxBundle/DataFixtures/ORM/Migrations.php
+++ src/Libbit/LoxBundle/DataFixtures/ORM/Migrations.php
@@ -32,9 +32,9 @@ class Migrations extends AbstractFixture implements OrderedFixtureInterface, Con
             // There is no migrations table yet.
             $connection->exec(
                 'CREATE TABLE IF NOT EXISTS `migration_versions` (' .
-                '`version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,' .
+                '`version` varchar(255) NOT NULL,' .
                 'PRIMARY KEY (`version`)' .
-                ') ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;'
+                ') ENGINE=InnoDB DEFAULT CHARSET=utf8;'
             );
         }
 
