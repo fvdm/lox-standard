a7d69505eb14360a3f7ca7779b60cfe24e4257ac
Nido Media <nido@foxserver.be>
2015-01-30 11:27:35 +0100

    removed newlines from certfile

--- src/Libbit/LoxBundle/Controller/WebController.php
+++ src/Libbit/LoxBundle/Controller/WebController.php
@@ -117,7 +117,7 @@ class WebController extends Controller
                 'application_logo' => $settings->getApplicationLogo(),
                 'app_fontcolor' => $settings->getAppFontcolor(),
                 'app_backcolor' => $settings->getAppBackcolor(),
-                'pin_cert' => $this->container->getParameter('pin_cert'),
+                'pin_cert' => str_replace(array("\n", "\r"), '', file_get_contents($this->container->getParameter('pin_cert'))),
             ),
             $response
         );
