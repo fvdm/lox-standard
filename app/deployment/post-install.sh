72058668a436b77d3e1f2b133cef2e7cfa35f2a7
Nido Media <nido@foxserver.be>
2015-02-11 14:56:46 +0100

    post-install2/update2-> post-update/install

--- app/deployment/post-install.sh
+++ app/deployment/post-install.sh
@@ -1,38 +1,42 @@
 #!/bin/sh
+RELDIR=$(readlink -f $(dirname "${0}"))
+cd "${RELDIR}/../.."
 
-# Initializes a new installation. This script should only be executed after a clean system install.
-# The app/config/parameters.yml file should be configured before executing this script.
+if 
+  app/console doctrine:query:sql "select * from libbit_lox_link"
+then
+  echo You already seem to have localbox installed on that database.
+  echo Please check app/config/parameters.yml and try again or run
+  echo post-update.sh instead
 
-# This script uses 777 permissions on the writable dirs for platform portability.
-# For more restrictive permissions, use something like ACL, depending on your platform. Write permissions are
-# required for both the console user and the apache user.
+else
+  if 
+    app/console doctrine:query:sql "select 1"
+  then
+    # Create the database.
+    app/console doctrine:database:create
 
-# Make sure only root can run our script
-if [ `id -u` -ne 0 ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+    # Create the database schema.
+    app/console doctrine:schema:create -q
+
+    # Load the fixtures.
+    app/console doctrine:fixtures:load -n
+
+    # Install assets.
+    app/console --env=prod assets:install --symlink web
+
+    # Install YUI assets.
+    app/console --env=prod rednose:yui:install
+
+    # Warm the cache.
+    app/console --env=prod cache:warm
+
+    chown -R apache .
+  else
+    echo Unable to connect to the database defined in the file
+    echo app/config/parameters.yml. Please check your settings, check
+    echo if the database is indeed up, and try again.
+
+  fi
+
 fi
-
-# Set permissions on the writable folders.
-chmod -R 777 app/cache app/logs data
-
-# Create the database.
-app/console doctrine:database:create
-
-# Create the database schema.
-app/console doctrine:schema:create -q
-
-# Load the fixtures.
-app/console doctrine:fixtures:load -n
-
-# Install assets.
-app/console --env=prod assets:install --symlink web
-
-# Install YUI assets.
-app/console --env=prod rednose:yui:install
-
-# Warm the cache.
-app/console --env=prod cache:warm
-
-# Reinitialize permissions on the cache and data folder.
-chmod -R 777 app/cache/ data
--- app/deployment/post-update.sh
+++ app/deployment/post-update.sh
@@ -1,31 +1,26 @@
-#!/bin/sh
+#!/bin/sh -x
+# Initializes an existing installation. This script should be executed
+# automatically after updating an installation.
+RELDIR=$(readlink  -f $(dirname "${0}")/../..)
+cd "${RELDIR}"
 
-# Initializes an existing installation. This script should be executed after updating an installation.
+DBNAME=`cat app/config/parameters.yml | grep database_name: | sed "s/ *database_name: *//g"`
+SIZ=`app/console doctrine:query:sql "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$DBNAME' and TABLE_TYPE='BASE TABLE'" | grep "'COUNT(\*)' => string '0' (length=1)"`
 
-# This script uses 777 permissions on the writable dirs for platform portability.
-# For more restrictive permissions, use something like ACL, depending on your platform. Write permissions are
-# required for both the console user and the apache user.
-
-# Make sure only root can run our script
-if [ `id -u` -ne 0 ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+if
+  test -z "$SIZ" 
+then
+  sudo -u apache app/console --env=prod cache:clear
+  app/console --env=prod doctrine:migrations:migrate -n
+  app/console --env=prod assets:install --symlink web
+  app/console --env=prod rednose:yui:install
+  sudo -u apache app/console --env=prod cache:warm
+else
+  cat > /dev/stderr <<EOF
+The update script is unable to find the old database, or said database
+is empty. If this is a new install, that is to be expected. If not,
+please check if the app/config/parameters.yml has the right
+host/port/user/pass. If this is a new install, please run post-install
+instead
+EOF
 fi
-
-# Set permissions on the cache folder.
-chmod -R 777 app/cache
-
-# Clear the current cache and warmup a new version.
-app/console --env=prod cache:clear
-
-# Execute database migrations.
-app/console --env=prod doctrine:migrations:migrate -n
-
-# Install assets.
-app/console --env=prod assets:install --symlink web
-
-# Install YUI assets.
-app/console --env=prod rednose:yui:install
-
-# Reinitialize permissions on the cache folder
-chmod -R 777 app/cache
